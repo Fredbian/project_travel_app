b4c32064f54307b6cb355fb2af4abaa303c62a60
Tao Bian <biantao0519@hotmail.com>
2023-04-14 15:13:33 +1000

    updated home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,7 +3,9 @@ import './index.css'
 
 const Home = () => {
   return (
-    <div className="home-container">
+    <div className="home-container" 
+    style={{ backgroundImage: `url(/Travel.png)`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundSize: 'contain', height: '90vh', width: '90vw' }}
+    >
       <h1 className="title">Want to make a travel plan?</h1>
       <Link to={'/travelapp'} className='link'>
         <button className="home-btn">Let's Go!!!</button>
